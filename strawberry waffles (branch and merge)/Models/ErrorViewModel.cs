de7dd063d05c2ae9a56629c7dc47971b7d823c23
Sneha Sil <snehasil413@gmail.com>
2021-06-13 14:52:48 -0500

    created project

---
+++ strawberry waffles (branch and merge)/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace strawberry_waffles__branch_and_merge_.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
